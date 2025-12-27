7d6f8071f4d6a9242ccde4b418ea5f663a0e311e
ehddnjs1747 <39339119-ehddnjs1747@users.noreply.replit.com>
2025-12-27 07:59:45 +0000

    Initial commit

---
+++ src/com/leaveplanner/domain/LeaveDay.java
@@ -0,0 +1,12 @@
+
+import jave.time.localDate;
+
+public class LeaveDay{
+  
+  private final Long leaveDayid;
+  private final Long userId;
+  private final LeaveType subType;
+  private LocalDate date;
+  private final String memo;
+  
+}
