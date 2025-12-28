64d71dc6be0e9ab8c8240478bfea4013c8fe3242
Chu DongWon <100004234+chu-dw@users.noreply.github.com>
2025-12-28 05:42:23 +0000

    feat: set up LeaveDay domai

--- src/com/leaveplanner/domain/LeaveDay.java
+++ src/com/leaveplanner/domain/LeaveDay.java
@@ -1,12 +1,22 @@
 
+import java.time.LocalDate;
+
 import jave.time.localDate;
 
 public class LeaveDay{
   
-  private final Long leaveDayid;
+  private final Long leaveDayId;
   private final Long userId;
   private final LeaveType subType;
   private LocalDate date;
   private final String memo;
+
+  public LeaveDay(Long leaveDAyId, Long userId, LeaveType subType, LocalDate date, String memo){
+    this.leaveDAyId = leaveDAyId;
+    this.userId = userId;
+    this.subType = subType;
+    this.date = date;
+    this.memo = memo;
+  }
   
 }
---
+++ src/com/leaveplanner/domain/LeaveType.java
@@ -0,0 +1,14 @@
+public class LeaveType{
+
+  private final Long leaveTypeId;
+  private final String name;
+  private final boolean isCustom;
+
+  public LeaveType(Long leaveTypeId, String name, boolean isCustom){
+    this.leaveTypeId = leaveTpeId;
+    this.name = name;
+    this.isCustom = isCustom;
+  }
+
+  
+}
