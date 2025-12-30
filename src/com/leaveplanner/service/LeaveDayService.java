9e17100f61e50f0e821ecd19f35d25a011d96d82
Chu DongWon <100004234+chu-dw@users.noreply.github.com>
2025-12-30 13:40:54 +0000

    feat: LeaveDayService

--- src/com/leaveplanner/service/LeaveDayService.java
+++ src/com/leaveplanner/service/LeaveDayService.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import src.com.leaveplanner.domain.LeaveDay;
 
 public class LeaveDayService{
 
@@ -10,11 +11,21 @@ public class LeaveDayService{
 
   //휴가 날짜 생성
   public void create(Long userId, LocalDate date, LeaveType leaveType, String memo){
-    
+
+    //해당 날짜 휴가존재 예외처리
+    if (leaveDayRepository.existsByUserIdAndDate(userId,date)){
+      throw new IlleagalStateException("이미 해당 날짜 휴가 존재");
+    }
+
+    //leaveDay 생성
+    LeaveDay leaveDay = new LeaveDay(userId, leaveType, memo);
+    //repository로
+    leaveDayRepository.svae(leaveDay);
   }
 
+  //한달치 휴가 조회
   public List<LeaveDay> getMonthlyLeaves(Long userId, LocalDate date){
-    
+    return leaveDayRepository.findByuserIdAndMonth(userId, month);
   }
   
 }
